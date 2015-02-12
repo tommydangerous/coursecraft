10913b39020d7d554276507e347f2f6a659dc979
Tommy Dang <quantumventuress@gmail.com>
2015-02-12 09:24:20 -0800

    readded asset host

--- config/initializers/asset_host.rb
+++ config/initializers/asset_host.rb
@@ -1,5 +1,5 @@
-# if Rails.env.production?
-#   # The // is necessary so that 'protocol-relative' urls are written,
-#   # and thus work on both HTTP and HTTPS pages.
-#   ActionController::Base.asset_host = "//d1lp148k2o3gqu.cloudfront.net"
-# end
+if Rails.env.production?
+  # The // is necessary so that 'protocol-relative' urls are written,
+  # and thus work on both HTTP and HTTPS pages.
+  ActionController::Base.asset_host = "//d1lp148k2o3gqu.cloudfront.net"
+end
